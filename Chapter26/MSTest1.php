0678bd57707c30efc3b6476ff5552d07606bc515
suikajy <2058550226@qq.com>
2018-05-17 14:41:04 +0800

    chapter 26 mysql

---
+++ Chapter26/MSTest1.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/5/13
+ * Time: 16:48
+ */
+
+$result = 0;
---
+++ JsTest/JSTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/5/15
+ * Time: 13:29
+ */
+
+?>
+
+<html>
+<head>
+
+    <title> js test 1 </title>
+
+    <script type="text/javascript">
+        console.log("log guoxu");
+        // var advName = prompt("请输入广告名", "please input adv name.");
+        // if (advName != null && advName !== "") {
+        //     document.write(advName);
+        // }
+    </script>
+
+</head>
+
+<body>
+test
+
+<form>
+    <input type="file"/>
+
+</form>
+</body>
+</html>
