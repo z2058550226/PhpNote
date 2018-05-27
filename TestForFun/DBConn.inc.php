e3806ae525bfee90c936339c3011e416e32fd9d8
suikajy <2058550226@qq.com>
2018-05-27 22:16:32 +0800

    merge home code

--- TestForFun/DBConn.inc.php
+++ TestForFun/DBConn.inc.php
@@ -15,7 +15,11 @@ function connectDB()
 {
     header("Content-type:text/html;charset=utf-8");//字符编码设置
 
-    $conn = mysqli_connect("119.3.23.148", "root", "ZJY@123456a", "zjy", "8635");
+    $conn = mysqli_connect("119.3.23.148",
+        "root",
+        "ZJY@123456a",
+        "zjy",
+        "8635");
 
     if (!$conn) {
         die("链接数据库错误" . mysqli_connect_error());
