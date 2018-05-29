56dd28b000faaa7ddbda220d92677aaaa5b5807d
suikajy <2058550226@qq.com>
2018-05-29 10:27:39 +0800

    delete the home code

--- Chapter10/FileTest4.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/5/1 0001
- * Time: 下午 16:51
- */
---
+++ Chapter31_PDO/PDOTest1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/5/29
+ * Time: 10:27
+ */
